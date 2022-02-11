b4595258b4786f1ef65545ecdf0754c3366abf97
Paul Bodner <bodner80@gmail.com>
2022-02-10 16:00:33 -0800

    refactored addProperty with postgres sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -170,9 +170,35 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const p = property
+  const values = [
+    p.owner_id,
+    p.title,
+    p.description,
+    p.thumbnail_photo_url,
+    p.cover_photo_url,
+    p.street,
+    p.city,
+    p.province,
+    p.post_code,
+    p.country,
+    p.parking_spaces,
+    p.number_of_bathrooms,
+    p.number_of_bedrooms
+  ];
+  return pool.query(`INSERT INTO properties
+                   (owner_id, title, description, thumbnail_photo_url,
+                    cover_photo_url, street, city, province, post_code,
+                    country, parking_spaces, number_of_bathrooms,
+                    number_of_bedrooms)
+                   Values ($1, $2, $3, $4, $5, $6,
+                    $7, $8, $9, $10, $11, $12, $13) 
+                   RETURNING *;`, values)
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    console.log('ERROR: ',err);
+  })
 }
 exports.addProperty = addProperty;
